dc321fce5ed768cc42db925769531a5e77f16283
RC Prasad <rcprasad.2000@gmail.com>
2025-08-21 01:16:53 +0530

    backend-course

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const express = require("express");
 const app = express();
 
 
-
+app.post("/user/signup" , function (req,res){
+    res.json({
+        message : "signup endoint"
+    })
+})
 
 
 
