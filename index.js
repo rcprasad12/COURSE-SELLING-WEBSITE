da9248b553c89a0464c54bf8c7775a4c012ae1b9
RC Prasad <rcprasad.2000@gmail.com>
2025-08-21 05:01:16 +0530

    Added Routers

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { userRouter } = require("./routes/user");
-const { courseRouter } = require("/routes/course");
+const { courseRouter } = require("./routes/course");
 
 const app = express();
 
