a703ae50417a2453519bf9d1b247446ef8a22e7c
RC Prasad <rcprasad.2000@gmail.com>
2025-08-21 23:24:21 +0530

    Added Routers

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course");
+const { adminRouter }  = require("./routes/admin");
+
+
 
 const app = express();
 
 
 app.use("/api/vi/user" , userRouter);
 app.use("/api/v1/course" , courseRouter);
-
-
+app.use("/api/v1/admin" ,adminRouter);
 
 
 app.use(express.json());
---
+++ routes/admin.js
@@ -0,0 +1,39 @@
+const { Router } = require("express");
+const adminRoouter = Router();
+
+adminRoouter.post("/signup" , function (req,res){
+    res.json({
+        message : "signup endoint"
+    })
+})
+
+
+adminRouter.post("/signIn" , function (req,res){
+    res.json({
+        message : "signIn endoint"
+    })
+})
+
+adminRouter.post("/course" , function (req,res){
+    res.json({
+        message : "To create a course"
+    })
+})
+
+adminRouter.put("/course" , function (req,res){
+    res.json({
+        message : "To ADD a course"
+    })
+})
+
+
+adminRouter.get("/course/bulk" , function(req,res){
+    res.json({
+        mesaage : "Sifnn endpoint"
+    })
+})
+
+
+module.exports = {
+    adminRouter : adminRouter
+}
