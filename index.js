b0cf3b44688813cbdf11e82caeb33870f973d119
RC Prasad <rcprasad.2000@gmail.com>
2025-08-23 04:10:17 +0530

    signup verified in postman

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course");
 const { adminRouter }  = require("./routes/admin");
 
- app.use(express.json());
+ 
 
 const app = express();
+
+app.use(express.json());
 
 
 app.use("/api/v1/user" , userRouter);
@@ -20,10 +22,17 @@ console.log("Loaded URI:", process.env.MONGO_URI); // Debugging line
 
 
 async function main() {
-    
+        
+        try{
         await mongoose.connect(process.env.MONGO_URI);
        
-        app.listen(3000);
+        app.listen(3000 , () =>{
+                console.log("Server running on http://localhost:3000");
+        });
+        }
+        catch(error){
+                console.log("MongoDB connection error : " , error);
+        }
 }
 
 main();
